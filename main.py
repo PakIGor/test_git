d1e2347f992928d5a0d348a07409168c82834ee2
pak_igor <forstudy20050607@gmail.com>
2024-09-02 21:35:47 +0500

    F/n feature

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ for i in range(10):
 
 print("God always hear your prays")
 
+def feature():
+    while True:
+        print("God always with you!")
+
 def main():
     for i in range(10):
         print("God is Love!")
